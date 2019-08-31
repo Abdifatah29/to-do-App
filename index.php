ed75976e49655b257d0ccf7149aac5c363529289
Abdifatah29 <abdi.hassannoor01@gmail.com>
2019-08-31 21:15:50 +0200

    generated list dynamically

--- index.php
+++ index.php
@@ -1,20 +1,38 @@
+<?php
+session_start();
+if(!isset($_SESSION['list'])) {
+    $_SESSION['list'] = array();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>To-do App</title>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+ <!-- Link to stylesheet CSS -->
+ <link rel="stylesheet" type="text/css" href="css/styles.css">
+ <title>To do list</title>
 </head>
 <body>
-    <form action="index.php" method="post">
-        <input type="text" name="enterList">
-		<button type="submit">Add</button>
-    </form>
-    <?PHP 
-        if($_POST) {
-            echo "<br>" . "\n <ul> \n <li> " . $_POST("enterList") . "</li> \n </ul>";
-        }    
-    ?>
+     
+     <form action="index.php" method="post">
+         <input type="text" name="todoEntry" id="name" >
+         <button type="submit" >Add</button>
+     </form>
+ <?php
+    if (isset($_POST['todoEntry'])) {
+        // var_dump($_POST['todoEntry']);
+            $_SESSION['list'][] = $_POST['todoEntry'];
+            // var_dump($_SESSION['list']);
+        } if (isset($_SESSION['list'])) {
+            foreach ($_SESSION['list'] as $item) {
+                echo  $item ."</br>";
+            }
+        }
+            // var_dump($_SESSION[]);
+
+ ?>
 </body>
 </html>
